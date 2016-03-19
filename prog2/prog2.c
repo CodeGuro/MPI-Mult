f83f675a80f7971c8a3f42f6d383e761657da2bf
Tony Lane <CodeGuro@gmail.com>
2016-03-19 13:10:54 -0400

    progressing...

--- prog2/prog2.c
+++ prog2/prog2.c
@@ -41,8 +41,8 @@ int main( int argc, char **argv )
 		exit( 1 );
 	}
 
-	int sub_size = n / p; // size of the submatrices
-	int steps = n / sub_size; // number of submatrices to evaluate per process
+	const int sub_size = n / p; // size of the submatrices
+	const int steps = n / sub_size; // number of submatrices to evaluate per process
 
 	// allocate the sub-matrices
 	matrix_t *sub1 = alloc_mat( sub_size, NULL );
@@ -54,17 +54,40 @@ int main( int argc, char **argv )
 		for( int j = 0; j < steps; ++j )
 		{
 			zero_mat( sub3 );
-			for( int k = 0; k < steps; ++k )
+			float *mat_row_m1;
+			float *mat_col_m2;
+			float *adding_mat_set;
+			if( rank == 0 )
 			{
-				// non-parallel part
-				get_sub_mat( sub1, m1, i, k );
-				get_sub_mat( sub2, m2, k, j );
-				matrix_t *tmp = alloc_mat( sub_size, NULL );
-				multiply_mat( tmp, sub1, sub2 );
-				add_mat( sub3, sub3, tmp );
-				destroy_mat( tmp );
+				// copy the sub-matrices in rows/columns to distribute to each process
+				mat_row_m1 = malloc( sub_size*sub_size*steps * sizeof( float ) );
+				mat_col_m2 = malloc( sub_size*sub_size*steps * sizeof( float ) );
+				adding_mat_set = malloc( sub_size*sub_size*steps * sizeof( float ) );
+				for( int k = 0; k < steps; ++k )
+				{
+					get_sub_mat( sub1, m1, i, k );
+					copy_buff_mat( mat_row_m1, sub1, k );
+					get_sub_mat( sub2, m2, k, j );
+					copy_buff_mat( mat_col_m2, sub2, k );
+				}
 			}
-			set_sub_mat( m3, sub3, i, j );
+
+			// scatter the submatrices
+			MPI_Scatter( mat_row_m1, sub_size*sub_size*steps, MPI_FLOAT, sub1, sub_size*sub_size, MPI_FLOAT, 0, MPI_COMM_WORLD );
+			MPI_Scatter( mat_col_m2, sub_size*sub_size*steps, MPI_FLOAT, sub2, sub_size*sub_size, MPI_FLOAT, 0, MPI_COMM_WORLD );
+
+			// matrix multiplication is done locally
+			multiply_mat( sub3, sub1, sub2 );
+
+			// gather the submatrices into the root process
+			MPI_Gather( sub3, sub_size*sub_size, MPI_FLOAT, adding_mat_set, sub_size*sub_size*steps, MPI_FLOAT, 0, MPI_COMM_WORLD );
+
+			if( rank == 0 )
+			{
+				free( adding_mat_set );
+				free( mat_col_m2 );
+				free( mat_row_m1 );
+			}
 		}
 	}
 
